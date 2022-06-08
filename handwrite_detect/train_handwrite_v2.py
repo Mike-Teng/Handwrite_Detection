c41d6aeb067d4511cb2fc7a3ee20c5734d113631
Mike <m88215@gmail.com>
2022-06-08 18:44:22 +0800

    update .py

--- handwrite_detect/train_handwrite_v2.py
+++ handwrite_detect/train_handwrite_v2.py
@@ -14,8 +14,6 @@ def data_process(datapath):
     data_x = np.zeros((28,28)).reshape(1,28,28)
       
     for root, _ , files in os.walk(datapath):
-#         if files != ['.DS_Store']:
-#             print(root,files)
         if files != ['.DS_Store']:
             for f in files:
                 if f != ".DS_Store":
@@ -38,37 +36,43 @@ data_train_x, data_train_y = data_process(traindatapath)
 data_test_x, data_test_y = data_process(testdatapath)
 
 model = Sequential()
-model.add(Conv2D(64,kernel_size=(3,3),activation='relu',input_shape=(28,28,1)))
+model.add(Conv2D(32,kernel_size=(3,3),activation='relu',input_shape=(28,28,1)))
+model.add(Conv2D(32,kernel_size=(3,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32,kernel_size=(3,3),activation='relu'))
 model.add(Conv2D(64,kernel_size=(3,3),activation='relu'))
-model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(128,kernel_size=(3,3),activation='relu'))
-model.add(Conv2D(128,kernel_size=(3,3),activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-# model.add(Dropout(0.1))
+model.add(Dropout(0.1))
 model.add(Flatten())
 model.add(Dropout(0.1))
 
 model.add(Dense(128,activation='relu'))
-model.add(Dropout(0.25))
-model.add(Dense(64,activation='relu'))
-model.add(Dropout(0.25))
+model.add(Dropout(0.3))
 model.add(Dense(units=10,activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.8, patience=5, min_lr=0.00001)
-train_history = model.fit(data_train_x,data_train_y, batch_size=128, epochs=60, verbose=1, callbacks=[reduce_lr], validation_split = 0.2)
+reduce_lr = ReduceLROnPlateau(monitor='val_accuracy', factor=0.8, patience=6, min_lr=0.00001)
+train_history = model.fit(data_train_x,data_train_y, batch_size=32, epochs=80, verbose=1, callbacks=[reduce_lr], validation_split = 0.1)
 
 score = model.evaluate(data_test_x,data_test_y,verbose=0)
 print('Test loss:', score[0])
 print('Test acc:', score[1])
 
-# plt.plot(train_history.history['loss'])
-# plt.plot(train_history.history['val_loss'])
-# plt.plot(train_history.history['val_accuracy'])
-# plt.title('train history')
-# plt.show()
+plt.plot(train_history.history['loss'])
+plt.plot(train_history.history['val_loss'])
+plt.title('train history(loss)')
+plt.xlabel('Epoch')
+plt.ylabel('loss')
+plt.legend(['loss','val_loss'],loc = 'upper left')
+plt.show()
+
+plt.plot(train_history.history['accuracy'])
+plt.plot(train_history.history['val_accuracy'])
+plt.title('train history(acc)')
+plt.xlabel('Epoch')
+plt.ylabel('acc')
+plt.legend(['acc','val_acc'],loc = 'lower right')
+plt.show()
